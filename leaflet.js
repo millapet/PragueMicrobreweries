35abff90b10d1c26d9e3af22692da839c4bafd41
Petra Millarova <petramillarova@gmail.com>
2019-02-04 13:20:30 +0100

    create a separate *js file

--- leaflet.js
+++ leaflet.js
@@ -1,30 +1,4 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
-    <title>Prague Microbreweries</title>
-    <style>
-      html, body, #viewDiv {
-        padding: 0;
-        margin: 0;
-        height: 100%;
-        width: 100%;
-      }
-    </style>
-     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.4.0/dist/leaflet.css"
-   integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
-   crossorigin=""/>
-	 <script src="https://unpkg.com/leaflet@1.4.0/dist/leaflet.js"
-   integrity="sha512-QVftwZFqvtRNi0ZyCtsznlKSWOStnDORoefr1enyq5mVL4tmKB3S/EnC3rRJcxCPavG10IcrVGSmPh6Qw5lwrg=="
-   crossorigin="">
-   </script>
-   <script src="pivovary.geojson" type="text/javascript"></script>
-  </head>
-  <body>
-    <div id="mymap" style= "width: 100%; height: 100%" ></div>
-	<script type="text/javascript">
-		var map = L.map('mymap'); //.setView([50.082903, 14.424060], 12);
+var map = L.map('mymap'); //.setView([50.082903, 14.424060], 12);
 		//map.control.locate().addTo(map);
 		//var map = L.map('mymap');
 		//map.locate({setView: true, maxZoom: 16});
@@ -72,7 +46,3 @@
 				return L.marker(latlng, {icon: geojsonMarker});},
 			onEachFeature: onEachFeature
 		}).addTo(map);
-		
-	</script>
-  </body>
-</html>
