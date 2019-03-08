bf78f39721ea1627a2c316ff5b09af377371f088
Petra Millarova <petramillarova@gmail.com>
2019-03-08 13:40:50 +0100

    location button finished

--- microbreweries.js
+++ microbreweries.js
@@ -24,15 +24,10 @@ var nearestBeerMarker = L.icon({
 		
 var youAreHereMarker = L.icon({
 	iconUrl: 'https://img.icons8.com/doodle/42/000000/chevron-down.png',
+	//iconUrl: 'https://img.icons8.com/office/42/000000/map-pin.png',
 	iconAnchor: [21,42],
 	popupAnchor: [0,-42]
 });
-
-var getLocationMarker = L.divIcon({className:'leaflet-div-icon'});
-	//iconUrl: '<img src="https://img.icons8.com/plasticine/42/000000/place-marker.png">',
-	//iconAnchor: [21,21],
-	//popupAnchor: [0,-21]
-//});
 
 //GEOJSON LAYER
 //----------
@@ -62,21 +57,23 @@ var ourCustomControl = L.Control.extend({
 	//container.marker = L.marker(e.latlng, {icon: getLocationMarker});
 	
     //container.style.background = 'URL(\'https://img.icons8.com/plasticine/30/000000/place-marker.png\')';
-	container.style.backgroundImage = 'URL(\'https://img.icons8.com/ios/15/000000/center-direction.png\')';
+	container.style.backgroundImage = 'URL(\'https://img.icons8.com/metro/15/000000/center-direction.png\')';
 	container.style.backgroundColor = 'white';
 	container.style.backgroundRepeat = 'no-repeat'
 	container.style.backgroundPosition = 'center';
     container.style.width = '30px';
     container.style.height = '30px';
+	container.style.cursor = 'pointer';
 	
-	
- 
     container.onclick = function(){
 		map.locate({setView: true, maxZoom: 16});
       console.log('buttonClicked');
     }
-	container.ontouch = function(){
-		container.style.backgroundColor = 'gray';
+	container.onmouseover = function(){
+		container.style.backgroundColor = '#eee';
+    }
+	container.onmouseout = function(){
+		container.style.backgroundColor = 'white';
     }
     return container;
   },
@@ -87,12 +84,18 @@ map.addControl(new ourCustomControl());
 //USER LOCATION
 //----------
 map.setView([50.082903, 14.424060], 12)
+var markerLoc = null;
+var markerBeer = null;
 function onLocationFound(e){
-	L.marker(e.latlng, {icon: youAreHereMarker}).addTo(map),
+	if(markerLoc != null && markerBeer != null){
+		map.removeLayer(markerLoc);
+		map.removeLayer(markerBeer)
+	}
+	markerLoc = L.marker(e.latlng, {icon: youAreHereMarker}).addTo(map),
 	//.bindPopup('<font size="3"> You are here </font>'+L.GeometryUtil.closestLayer(map, geojsonLayer.getLayers(), e.latlng).latlng).openPopup(),
 	closestPt = L.GeometryUtil.closestLayer(map, geojsonLayer.getLayers(), e.latlng),
 	ID = L.stamp(closestPt),
-	L.marker(closestPt.latlng,{icon: nearestBeerMarker}).addTo(map)
+	markerBeer = L.marker(closestPt.latlng,{icon: nearestBeerMarker}).addTo(map)
 	.bindPopup('<font size="3"><p><b>'+closestPt.layer.feature.properties.name+'</b></font><br /><font size="3">'+closestPt.layer.feature.properties.address+'<br /> <a href="'+closestPt.layer.feature.properties.web+'">'+closestPt.layer.feature.properties.web+'</a></p></font>')
 }
 function onLocationError(e) {
